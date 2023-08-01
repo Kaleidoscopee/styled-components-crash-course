e3314c845fad0baf64f6a48bd40098923b77514a
Kaleidoscopee <133397937+Kaleidoscopee@users.noreply.github.com>
2023-08-01 06:42:52 -0400

    header updated 27:49

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -12,3 +12,17 @@ export const StyledHeader = styled.header`
     //     background-color: black;
     // }
 `
+
+export const Nav = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 40px;
+`
+
+export const Logo = styled.img``
+
+export const Image = styled.img`
+    width: 375px;
+    margin-left: 40px;
+`
